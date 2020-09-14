5c9b098a4efa85cfb624cd8cc0a5762b952bf4e1
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-09-14 15:39:42 +0300

    🚧 Part 1 exercise course information (finish ?)

---
+++ part1/course-information/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            {props.parts.map((part, i) => {
+                return (<Part key={i} part={part}/>)
+            })}
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.parts.map(p => p.exercises).reduce((prev, next) => prev + next)}</p>
+    )
+}
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
